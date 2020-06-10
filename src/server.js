cdee56b0e50a5c41fb1259644f23752c90a1f908
Mr Raeisi <MrRaeisi@outlook.com>
2020-06-10 15:17:13 +0430

    Add cors entry  + Register cors plugin  + Register API routes as a sub app under /api/v1 route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Boom = require('@hapi/boom')
 const express = require('express')
+const cors = require('cors')
 const config = require('./config')
 const path = require('path')
 const registerAPI = require('./api')
@@ -8,12 +9,16 @@ const errorHandler = require('./middlewares/error-handler')
 global.$logger = config.loggers.defaultLogger
 
 const server = new express()
+server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use(express.static(path.join(__dirname, 'public')))
 server.use(config.loggers.expressLogger)
 
-registerAPI(server)
+// Register API routes
+const apiContext = new express()
+registerAPI(apiContext)
+server.use('/api/v1', apiContext)
 
 server.all('*', function (req, res, next) {
   next(Boom.notFound())
@@ -23,5 +28,5 @@ server.all('*', function (req, res, next) {
 server.use(errorHandler)
 
 server.listen(config.server.port, function () {
-  console.log(`Server is running on ${config.server.port} port`)
+  console.log(`Server is running on ${ config.server.port } port`)
 })
